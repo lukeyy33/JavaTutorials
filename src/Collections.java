548e5764d81cc731cca56d718a9c70aabfd1f762
Luke Waugh <b3030652@my.shu.ac.uk>
2016-07-28 09:13:31 +0100

    Added second method to edit items in an array list

--- src/Collections.java
+++ src/Collections.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 
 public class Collections {
+	
+	static ArrayList<String> list = new ArrayList<String>();
+	static ArrayList<String> list2 = new ArrayList<String>();
+
 	public static void main (String[] args) {
-		
+		arrayList();
+		editList();
+
+	}
+	
+	public static void arrayList() {
 		String[] things = {"eggs", "lasers", "hats", "pie"};
-		ArrayList<String> list = new ArrayList<String>();
+		String[] morethings = {"lasers", "hats"};
 		
 		//add array items to list
 		for (String x: things)
-			list.add(x);
-		
-		String[] morethings = {"lasers", "hats"};
-		ArrayList<String> list2 = new ArrayList<String>();
+			list.add(x);		
 		
 		for (String y: morethings)
 			list2.add(y);
@@ -20,4 +29,28 @@ public class Collections {
 			System.out.printf("%s ", list.get(i));			
 		}
 	}
+
+	public static void editList() {
+		
+		editTheList(list,list2);
+		System.out.println();
+		
+		for (int i = 0; i < list.size(); i ++) {
+			System.out.printf("%s ", list.get(i));			
+		}
+	}
+
+	public static void editTheList(Collection<String> l1, Collection<String> l2) {
+		Iterator<String> it = l1.iterator();
+		//Loop through the list item by item
+		while(it.hasNext()) {
+			//if item from l1 is in l2, it's removed
+			if (l2.contains(it.next()))
+					it.remove();
+		}
+		
+	}
+
+
+
 }
