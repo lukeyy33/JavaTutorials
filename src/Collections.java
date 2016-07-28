ed750bf82e2d34cf446bcd79df6482225d2efca1
Luke Waugh <b3030652@my.shu.ac.uk>
2016-07-28 08:55:29 +0100

    Adding to an array list

--- src/Collections.java
+++ src/Collections.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class Collections {
-
+	public static void main (String[] args) {
+		
+		String[] things = {"eggs", "lasers", "hats", "pie"};
+		ArrayList<String> list = new ArrayList<String>();
+		
+		//add array items to list
+		for (String x: things)
+			list.add(x);
+		
+		String[] morethings = {"lasers", "hats"};
+		ArrayList<String> list2 = new ArrayList<String>();
+		
+		for (String y: morethings)
+			list2.add(y);
+		
+		for (int i = 0; i < list.size(); i ++) {
+			System.out.printf("%s ", list.get(i));			
+		}
+	}
 }
